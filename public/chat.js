00e64e3b03898d48e4d1e8001129fbda1ce25484
Chichi <xuruz111@gmail.com>
2024-11-30 21:04:44 -0500

    wss

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // 改成本地的IP地址
-const socket = io("ws://chinese-culture.onrender.com:3000");
+const socket = io("wss://chinese-culture.onrender.com");
 
 socket.on("chat message", (msg) => {
   chatMatterMgr.add(msg.message);
